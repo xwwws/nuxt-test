86cd362c8cf02da5048bf912ff5c4fba55b27a0a
wz <17600820823@163.com>
2026-01-14 09:31:03 +0800

    添加element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
     '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@element-plus/nuxt',
+    '@nuxtjs/color-mode'
   ],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: [
-    
     '@unocss/reset/tailwind.css',
     '~/assets/style/scss/init.scss',
   ]
